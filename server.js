062f9fd7241d2e4a9c222b510dedee66e04eafe2
jayedmonson <jayedmonson@westcliff.edu>
2024-10-07 21:39:35 -0500

    classwork complete

--- server.js
+++ server.js
@@ -59,6 +59,20 @@ app.get('/home',
   function(req, res) {
     res.render('home', { user: req.user });
   });
+app.get('/login',
+  function(req, res) {
+    res.render('login');
+  });
+app.post('/login',
+  passport.authenticate('local', {failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+app.get('/logout',
+  function(req, res) {
+    req.logout();
+    res.redirect('/');
+  });
 
 
 app.listen(3000);
